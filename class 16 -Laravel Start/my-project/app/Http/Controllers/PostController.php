9cc912076103625ef2f4cf1c286a856b53edf46e
Pollob Ray <pollobroy978@gmail.com>
2024-05-22 18:05:48 +0600

    class 16 7

--- class 16 -Laravel Start/my-project/app/Http/Controllers/PostController.php
+++ class 16 -Laravel Start/my-project/app/Http/Controllers/PostController.php
@@ -10,6 +10,7 @@ class PostController extends Controller
     public function index() //created by ray which is 
     {
         $posts=Post::all(); // all files from Post
-        dd($posts);// to debug
+        //dd($posts);// to debug
+        return view("posts",compact("posts")); //first posts is view post.blade and second is the varibale
     }
 }
---
+++ class 16 -Laravel Start/my-project/resources/views/posts.blade.php
@@ -0,0 +1,7 @@
+
+@extends('layout.master') 
+<!-- import code from layout/master.blade.php code -->
+
+@section('content') <!-- push some code into at yeild('content') of master.blade --> 
+   <h2>Hello World</h2> 
+@endsection

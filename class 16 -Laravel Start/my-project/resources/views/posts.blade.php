02de6939d81b63632f5fc1a2841684b54ffaf0f1
Pollob Ray <pollobroy978@gmail.com>
2024-05-22 18:19:37 +0600

    class 16 8

--- class 16 -Laravel Start/my-project/resources/views/posts.blade.php
+++ class 16 -Laravel Start/my-project/resources/views/posts.blade.php
@@ -3,5 +3,28 @@
 <!-- import code from layout/master.blade.php code -->
 
 @section('content') <!-- push some code into at yeild('content') of master.blade --> 
-   <h2>Hello World</h2> 
+   <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $item)<!-- start for--> <!-- posts variable from PostController-->
+                        <tr>
+                            <th scope="row">{{$item->id}}</th> <!-- viewing data from database -->
+                            <td>{{$item->title}}</td>
+                            <td>{{$item->description}}</td>
+                          </tr>
+                        @endforeach <!-- end for-->
+                    </tbody>
+                  </table>
+            </div>
+        </div>
+   </div>
 @endsection
